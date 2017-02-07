0310c5b9f3d539aa4c80dfcc42ef1b8bf83968a3
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-02-07 09:18:30 -0800

    refactored to resemble implementation of linkedList, hashtable, sets, and queues

--- sets.py
+++ sets.py
@@ -2,13 +2,13 @@ from hashtable import HashTable
 
 class Set(HashTable):
     def __init__(self, init_size=8):
-          super(Set, self).__init__()
+          self.hash = HashTable()
+          if iterable:
+              for item in iterable:
+                  self.add(item)
 
-    def bucket_index(self, key):
-        return super.bucket_index(super.self, key)
+    def __repr__(self):
+        return 'Set({})'.format(self.has.length())
 
     def length(self):
-        length = 0
-        for bucket in self.buckets:
-            length += bucket.length()
-        return length
+        return self.hash.length()
