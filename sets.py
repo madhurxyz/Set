20837f941e0dca24aab167bf0132cf02f31c72db
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-02-07 09:15:52 -0800

    added length function

--- sets.py
+++ sets.py
@@ -4,5 +4,11 @@ class Set(HashTable):
     def __init__(self, init_size=8):
           super(Set, self).__init__()
 
-      def bucket_index(self, key):
-          return super.bucket_index(super.self, key)
+    def bucket_index(self, key):
+        return super.bucket_index(super.self, key)
+
+    def length(self):
+        length = 0
+        for bucket in self.buckets:
+            length += bucket.length()
+        return length
