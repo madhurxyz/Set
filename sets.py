d253322a8a2b928c4d1ea4503cf631de068698a4
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-02-07 09:15:07 -0800

    added bucket_index and __init__ functions

--- sets.py
+++ sets.py
@@ -1,4 +1,8 @@
 from hashtable import HashTable
 
 class Set(HashTable):
-    
+    def __init__(self, init_size=8):
+          super(Set, self).__init__()
+
+      def bucket_index(self, key):
+          return super.bucket_index(super.self, key)
